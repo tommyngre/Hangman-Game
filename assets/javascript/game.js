6ade7ee872a891df8dbd1474fb2674add3576d45
unknown <tommyn.gre@gmail.com>
2018-03-04 15:31:17 -0500

    arrays store chars n key presses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,46 @@
-var words = ["Bimp", "Bramp"];
+var words = ["Bimp", "Bramp", "Priest"];
 
 var word = words[Math.floor(Math.random() * Math.floor(words.length))];
 //console.log(word);
 
 var charCount = word.length;
-console.log(word + " is " + charCount + " chars");
+//console.log(word + " is " + charCount + " chars");
+
+// build array of chars in var word
+var charAry = new Array;
+charAry = buildCharAry(word);
+function buildCharAry(word){
+for (i=0 ; i<charCount; i++){
+  charAry.push(word.charAt(i));
+  //console.log(charAry);
+}
+return charAry;
+}
+
+//add keys pressed by user to array
+var keysPressedAry = new Array;
 
 var lastKeyPress = document.getElementById("last-key-press");
 
+function keysPressedToString(keysPressedAry){
+  for (i=0; i<keysPressedAry.length; i++){
+  keysPressedString = keysPressedString + keysPressedAry[i];
+  }
+  console.log(keysPressedString);
+  return keysPressedString;
+}
+
+var keysPressedString = "";
+
+
 document.onkeyup = function(e){
   lastKeyPress.textContent = e.key;  
+  //check if unique alpha; if so, add to array
+  keysPressedAry.push(e.key);
+  //console.log(keysPressedAry);
+
+  //print keys pressed array to screen
+  keysPressed.textContent = keysPressedToString(keysPressedAry);
 };
 
 function htmlStr(charCount){
@@ -21,9 +52,12 @@ function htmlStr(charCount){
   return str;
 };
 
+var keysPressed = document.getElementById("keys-pressed");
+
 // console.log(placeholders);
 var charPlaceholders = document.getElementById("char-placeholders");
 charPlaceholders.innerHTML = htmlStr(charCount);
 
 var numLetters = document.getElementById("num-letters");
 numLetters.textContent = charCount;
+
